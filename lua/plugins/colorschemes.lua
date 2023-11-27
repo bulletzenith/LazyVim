827878e0a819a4392f9fa5045320b17b2b2b8149
bulletzenith <hexagon.tr0@gmail.com>
2023-11-27 18:02:35 +0300

    Enable displaying the bufferline at all times

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,19 @@
 return {
-	"navarasu/onedark.nvim",
-	"ellisonleao/gruvbox.nvim",
+	{ "navarasu/onedark.nvim" },
+
+	{ "ellisonleao/gruvbox.nvim" },
+
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+	-- new plugin to get all the popular colorschemes from github
+	{ "linrongbin16/colorbox.nvim", lazy = true },
+
+	-- nice black and white theme
+	{ "seandewar/paragon.vim" },
+
+	-- nice white and black theme
+	{ "Verf/deepwhite.nvim" },
+
+	-- i love minimalistic themes
+	{ "eihigh/vim-aomi-grayscale", name = "aomi-grayscale" },
 }
